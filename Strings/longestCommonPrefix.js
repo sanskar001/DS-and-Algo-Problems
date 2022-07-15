79c935bc70fb56430e294e6c6a50247ea2736acc
Sanskar Maheshwari <46653101+sanskar001@users.noreply.github.com>
2022-07-15 19:08:57 +0530

    Update longestCommonPrefix.js

--- Strings/longestCommonPrefix.js
+++ Strings/longestCommonPrefix.js
@@ -25,3 +25,22 @@ var longestCommonPrefix = function(strs) {
     
     return commonPrefix;
 };
+
+
+// Another Solution:
+
+var longestCommonPrefix = function(strs) {
+    
+    if(strs.length === 0) return "";
+    
+    let prefix = strs[0]
+    
+    for(let i = 1; i < strs.length; i++){
+        
+        while(!strs[i].startsWith(prefix)){
+            prefix = prefix.slice(0, prefix.length - 1);
+        }
+    }
+    
+    return prefix;
+};
