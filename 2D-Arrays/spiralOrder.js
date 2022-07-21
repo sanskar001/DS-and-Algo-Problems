c72429f9d02e5982c5feb0b768172e7911b3b3bc
Sanskar Maheshwari <46653101+sanskar001@users.noreply.github.com>
2022-07-21 09:51:01 +0530

    Update spiralOrder.js

--- 2D-Arrays/spiralOrder.js
+++ 2D-Arrays/spiralOrder.js
@@ -15,7 +15,7 @@ Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 
 */
 
-// Solution: Time complexity - ~ O(n2) Space complexity - O()
+// Solution: Time complexity -  O(m*n) Space complexity - O(1)
 
 var spiralOrder = function(matrix) {
     
