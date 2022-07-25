ef36dad45b922e773f3f32e716b7160ff3b13455
Sanskar Maheshwari <46653101+sanskar001@users.noreply.github.com>
2022-07-25 22:55:34 +0530

    Create productOfArray.js

---
+++ Recursion/Easy/productOfArray.js
@@ -0,0 +1,13 @@
+function productOfArray(arr){
+    if(arr.length === 0){
+        return 1;
+    }
+    
+    return arr[0] * productOfArray(arr.slice(1));
+}
+
+// Testing:
+
+console.log(productOfArray([1, 2, 3]));
+console.log(productOfArray([1, 0, 3]));
+console.log(productOfArray([1, -2, 3]));
